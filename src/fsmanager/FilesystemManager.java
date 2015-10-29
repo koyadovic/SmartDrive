a63059d2cb35ff9111ded0048ff8b6cb91cd8257
Miguel Collado <m.collado.gomez@gmail.com>
2015-10-29 10:23:34 +0100

    Added some Javadoc to explain some interfaces used.

--- src/fsmanager/FilesystemManager.java
+++ src/fsmanager/FilesystemManager.java
@@ -4,5 +4,8 @@ package fsmanager;
  * Created by user on 21/10/15.
  */
 public interface FilesystemManager {
+    /**
+     * @param operation The concrete operation that will be done, like copy, move, delete, etc.
+     */
     void operate(ManagerOperation operation);
 }
--- src/fsmanager/ManagerOperation.java
+++ src/fsmanager/ManagerOperation.java
@@ -9,12 +9,29 @@ public interface ManagerOperation {
     int STATUS_WORKING      = 2;
     int STATUS_FINALIZED    = 3;
 
+    /**
+     * @return STATUS_IDLE, STATUS_STARTING, STATUS_WORKING, STATUS_FINALIZED
+     */
     int getStatus();
+
+    /**
+     * @return The current status explained for the UI
+     */
     String getStatusText();
 
+    /**
+     * @param observer Object implementing the interface OperationObserver that will be notified with every STATUS change
+     */
     void addOperationObserver(OperationObserver observer);
+
+    /**
+     * @param observer Renove previous observer
+     */
     void removeOperationObserver(OperationObserver observer);
 
+    /**
+     * Do the job!
+     */
     void operate();
 
 }
